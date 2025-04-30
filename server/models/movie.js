86fcda3e6f1c7d2c0547f46b770b063691ca3db1
C-Kubiak <cdkpdt@umsystem.edu>
2025-04-30 17:50:13 -0500

    Basic File Structure

--- server/models/movie.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const movieSchema = new mongoose.Schema({
-  title: String,
-  ratings: [{ user: String, score: Number }]
-});
-
-module.exports = mongoose.model('Movie', movieSchema);
--- server/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
-
-module.exports = mongoose.model('User', userSchema);
